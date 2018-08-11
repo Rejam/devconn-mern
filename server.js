9a9f6321d79fbfb6d0b1f9e7ee08d2741a7170ca
Ryan James <rejames@gmail.com>
2018-08-11 14:19:46 +0100

    basic routing structure. auth, posts, profile

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//mongoose
+// import routes
+const authRoute = require("./routes/api/auth");
+const profileRoute = require("./routes/api/profile");
+const postsRoute = require("./routes/api/posts");
+
+// mongoose
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoDB;
+const options = { useNewUrlParser: true };
 mongoose
   .connect(
     db,
-    { useNewUrlParser: true }
+    options
   )
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-app.get("/:name?", (req, res) =>
-  res.send(`Hi ${req.params.name ? req.params.name : ""}`)
-);
+// use routes
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
+
+app.get("/", (req, res) => res.send(`Hello`));
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
