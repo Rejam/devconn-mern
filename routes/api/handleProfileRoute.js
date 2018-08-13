32f85253a97d447ada3f3805f78cf3f6a78eea12
Ryan James <rejames@gmail.com>
2018-08-13 10:03:35 +0100

    cleaned up profile route

---
+++ routes/api/handleProfileRoute.js
@@ -0,0 +1,69 @@
+// Validation
+const validate = require("../../validation");
+const profileValidation = require("../../validation/profile");
+
+// models
+const Profile = require("../../models/Profile");
+
+const updateProfile = () => async (req, res) => {
+  // Get profile data
+  const profile = { ...req.body };
+  profile.user = req.user.id;
+  // Skills - split csv into array
+  const { skills } = req.body;
+  if (skills) profile.skills = skills.split(",");
+  // validate
+  const { errors, isValid } = validate(profile, profileValidation);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const profileExists = await Profile.findOne({ user: req.user.id });
+  if (profileExists) {
+    const updatedProfile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: profile },
+      { new: true }
+    );
+    return res.json(updatedProfile);
+  } else {
+    // profile doesn't exist
+    // check handle is unique
+    const handleExists = await Profile.findOne({
+      handle: profile.handle
+    });
+    if (handleExists) {
+      // @ts-ignore
+      errors.handle = "Handle already in use";
+      return res.status(400).json(errors);
+    } else {
+      // Save new profile
+      const newProfile = await Profile.create(profile);
+      return res.json(newProfile);
+    }
+  }
+};
+
+const getCurrentProfile = (req, res) => {
+  const errors = {};
+  // pull user id from authenticated user
+  const { id } = req.user;
+
+  // try to find user's profile
+  Profile.findOne({ user: id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        // profile not found
+        errors.profile = "Profile not found";
+        return res.status(404).json(errors);
+      }
+      // profile found
+      return res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+};
+
+module.exports = {
+  updateProfile,
+  getCurrentProfile
+};
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-// Validation
-const validate = require("../../validation");
-const profileValidation = require("../../validation/profile");
-
-// models
-const Profile = require("../../models/Profile");
-const User = require("../../models/User");
+const handleProfileRoute = require("./handleProfileRoute");
 
 /**
  * @route GET api/profile
@@ -17,25 +11,7 @@ const User = require("../../models/User");
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const errors = {};
-    // pull user id from authenticated user
-    const { id } = req.user;
-
-    // try to find user's profile
-    Profile.findOne({ user: id })
-      .populate("user", ["name", "avatar"])
-      .then(profile => {
-        if (!profile) {
-          // profile not found
-          errors.profile = "Profile not found";
-          return res.status(404).json(errors);
-        }
-        // profile found
-        return res.json(profile);
-      })
-      .catch(err => res.status(404).json(err));
-  }
+  handleProfileRoute.getCurrentProfile
 );
 
 /**
@@ -46,42 +22,6 @@ router.get(
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    // Get profile data
-    const profile = { ...req.body };
-    profile.user = req.user.id;
-    // Skills - split csv into array
-    const { skills } = req.body;
-    if (skills) profile.skills = skills.split(",");
-    // validate
-    const { errors, isValid } = validate(profile, profileValidation);
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
-    const profileExists = await Profile.findOne({ user: req.user.id });
-    if (profileExists) {
-      const updatedProfile = await Profile.findOneAndUpdate(
-        { user: req.user.id },
-        { $set: profile },
-        { new: true }
-      );
-      return res.json(updatedProfile);
-    } else {
-      // profile doesn't exist
-      // check handle is unique
-      const handleExists = await Profile.findOne({
-        handle: profile.handle
-      });
-      if (handleExists) {
-        // @ts-ignore
-        errors.handle = "Handle already in use";
-        return res.status(400).json(errors);
-      } else {
-        // Save new profile
-        const newProfile = await Profile.create(profile);
-        return res.json(newProfile);
-      }
-    }
-  }
+  handleProfileRoute.updateProfile()
 );
 module.exports = router;
