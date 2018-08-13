3145e4547bf70947c27b703a3a7914116c38829c
Ryan James <rejames@gmail.com>
2018-08-13 11:39:01 +0100

    async/await unhandled errors issue. back to callbacks

--- routes/api/handleProfileRoute.js
+++ routes/api/handleProfileRoute.js
@@ -1,3 +1,4 @@
+// @ts-ignore
 // Validation
 const validate = require("../../validation");
 const profileValidation = require("../../validation/profile");
@@ -5,42 +6,44 @@ const profileValidation = require("../../validation/profile");
 // models
 const Profile = require("../../models/Profile");
 
-const updateProfile = () => async (req, res) => {
-  // Get profile data
+const updateProfile = () => (req, res) => {
+  // Get profile data from form
   const profile = { ...req.body };
   profile.user = req.user.id;
   // Skills - split csv into array
   const { skills } = req.body;
-  if (skills) profile.skills = skills.split(",");
+  profile.skills = skills ? skills.split(",") : [];
   // validate
   const { errors, isValid } = validate(profile, profileValidation);
   if (!isValid) {
     return res.status(400).json(errors);
   }
-  const profileExists = await Profile.findOne({ user: req.user.id });
-  if (profileExists) {
-    const updatedProfile = await Profile.findOneAndUpdate(
-      { user: req.user.id },
-      { $set: profile },
-      { new: true }
-    );
-    return res.json(updatedProfile);
-  } else {
-    // profile doesn't exist
-    // check handle is unique
-    const handleExists = await Profile.findOne({
-      handle: profile.handle
-    });
-    if (handleExists) {
-      // @ts-ignore
-      errors.handle = "Handle already in use";
-      return res.status(400).json(errors);
+  // Check if profile already exists for user
+  Profile.findOne({ user: req.user.id }).then(profileExists => {
+    if (profileExists) {
+      // Profile exists => update
+      Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profile },
+        { new: true }
+      ).then(updatedProfile => res.json(updatedProfile));
     } else {
-      // Save new profile
-      const newProfile = await Profile.create(profile);
-      return res.json(newProfile);
+      // profile doesn't exist
+      // check handle is unique
+      Profile.findOne({
+        handle: profile.handle
+      }).then(profileWithHandleExists => {
+        if (profileWithHandleExists) {
+          // @ts-ignore
+          errors.handle = "Handle already in use";
+          return res.status(400).json(errors);
+        } else {
+          // Save new profile
+          Profile.create(profile).then(newProfile => res.json(newProfile));
+        }
+      });
     }
-  }
+  });
 };
 
 const getCurrentProfile = (req, res) => {
@@ -51,14 +54,14 @@ const getCurrentProfile = (req, res) => {
   // try to find user's profile
   Profile.findOne({ user: id })
     .populate("user", ["name", "avatar"])
-    .then(profile => {
-      if (!profile) {
+    .then(currentProfile => {
+      if (!currentProfile) {
         // profile not found
         errors.profile = "Profile not found";
         return res.status(404).json(errors);
       }
       // profile found
-      return res.json(profile);
+      return res.json(currentProfile);
     })
     .catch(err => res.status(404).json(err));
 };
