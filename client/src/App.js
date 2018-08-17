4ac25274faea420ee78abe8d6ac8c8efa6bebaa0
Ryan James <rejames@gmail.com>
2018-08-17 01:25:18 +0100

    added react-router. created  /login and /register routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Landing} exact />
+            <div className="container">
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </div>
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
